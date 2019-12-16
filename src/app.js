97c4d46562d9d84e9dff2c739126258bbfe460a4
Sebastian Osyra <sebosy92@gmail.com>
2019-12-16 18:13:25 +0100

    Added CORS

--- src/app.js
+++ src/app.js
@@ -1,53 +1,22 @@
 const path = require('path')
 const express = require('express')
-const pollutedCities = require ('./utilis/cities')
-const wikIDescription = require('./utilis/description')
+const citiesRouter = require('./router/cities')
+const cors = require('cors')
+
 
 const app = express()
 const port = process.env.PORT
 
 const publicDirectoryPath =  path.join(__dirname, '../public')
 
+app.use(cors())
 app.use(express.json())
-
+app.use(citiesRouter)
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.send ('./public/index')
 })
-
-
-app.get ('/cities', (req, res) => {
-    if (!req.query.country){
-        return res.send ({ 
-            error: 'You must provide country!'
-        })
-    }
-
-    pollutedCities(req.query.country, (error, data) => {
-        if (error) {
-            console.log(error)
-            return res.send ({ error })
-        }
-        const cities = []
-        const numberOfCities = data.length
-        data.forEach((data) => {
-            wikIDescription(data.name, function (error, wikiData)  {
-                if (error) {
-                    res.send ({ error })
-                }
-                const object = {}
-                object.name = data.name
-                object.description = wikiData.extract
-                cities.push(object)
-                if (cities.length === numberOfCities){
-                    res.send(cities)
-                }
-            })
-        })
-    })
-})
-
 
 app.listen(port, ()=> {
     console.log('Server is up on port: ' + port)
---
+++ src/router/cities.js
@@ -0,0 +1,47 @@
+const express = require ('express')
+const router = new express.Router()
+
+const pollutedCities = require ('../utilis/cities')
+const wikIDescription = require('../utilis/description')
+
+
+
+router.get ('/cities', async (req, res) => {
+    
+    try {
+    
+        if (!req.query.country){
+            return res.status(400).send ({ 
+                error: 'You must provide country!'
+            })
+        }
+    
+        pollutedCities(req.query.country, (error, data) => {
+            if (error) {
+                return res.status(400).send ({ error })
+            }
+            const cities = []
+            const numberOfCities = data.length
+            data.forEach((data) => {
+                wikIDescription(data.name, function (error, wikiData)  {
+                    if (error) {
+                        res.send ({ error })
+                    }
+                    const object = {}
+                    object.name = data.name
+                    object.description = wikiData.extract
+                    cities.push(object)
+                    if (cities.length === numberOfCities){
+                        res.send(cities)
+                    }
+                })
+            })
+        })
+    } catch (e) {
+        res.status(500).send()
+    }
+
+})
+
+
+module.exports = router
